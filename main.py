fde71deeb733ac2034c85b40a0eb489936776766
Flowjo1979 <roger.jongen@ziggo.nl>
2025-10-26 00:21:56 +0200

    commit main

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ from constants import *
 
 
 def main():
+    # initialize pygame
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # infinte while loop wat zwart screen zal aanmaken waarbij event.type ervoor zorgt dat de afsluitbutton werkt
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
-    #print("Starting Asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
+
+
 
 
 if __name__ == "__main__":
