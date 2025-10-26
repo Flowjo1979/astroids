af38e5941c8e94101a1fcaa54b872676d76284a5
Flowjo1979 <roger.jongen@ziggo.nl>
2025-10-26 02:02:40 +0200

    player en circleshape aangemaakt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -8,13 +9,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2)
     # infinte while loop wat zwart screen zal aanmaken waarbij event.type ervoor zorgt dat de afsluitbutton werkt
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
 
         # begrens de framerate tot 60 FPS
         dt = clock.tick(60) / 1000
