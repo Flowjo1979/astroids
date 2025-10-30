3ce0cc987cb657bc36aff821701286f788e8b458
Flowjo1979 <roger.jongen@ziggo.nl>
2025-10-30 23:10:15 +0100

    player containers

--- main.py
+++ main.py
@@ -9,16 +9,23 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2)
     # infinte while loop wat zwart screen zal aanmaken waarbij event.type ervoor zorgt dat de afsluitbutton werkt
+    
     while (True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
        
         pygame.display.flip()
 
